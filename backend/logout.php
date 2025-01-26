8bf26f56bc6683e6ce9d147dbaa83bffecdbf04e
Sultanbek <dcbaalpvlape@gmail.com>
2025-01-26 11:10:37 +0500

    first

--- backend/logout.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-session_unset();  // Удаляем все переменные сессии
-session_destroy(); // Разрушаем сессию
-
-header("Location: ../index.php");  // Перенаправляем на главную страницу
-exit;
-?>
